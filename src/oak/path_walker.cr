79ccd8dac0bc87429c8f85b4082475d85006f13a
jwaldrip <jason@waldrip.net>
2020-06-25 14:08:56 -0600

    fix path walker

--- src/oak/path_walker.cr
+++ src/oak/path_walker.cr
@@ -1,14 +1,6 @@
 require "./walker"
 
 struct Oak::PathWalker < Oak::Walker
-  def dynamic_char?
-    {'*', ':', '(', ')'}.includes? current_char
-  end
-
-  def marker?
-    super || dynamic_char?
-  end
-
   def value(marker_count)
     size = size_until_marker(self.marker_count - marker_count)
     slice(pos, size).tap do |value|
